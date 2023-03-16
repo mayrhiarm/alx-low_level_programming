0153bfd1e02efd0d4ac543a73142976121366613
mayrhiarm <mariamsanni93@gmail.com>
2023-03-16 12:05:49 -0700

    Add printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -A printf function program
+ * main - A printf function progress
  * Return: 0
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
