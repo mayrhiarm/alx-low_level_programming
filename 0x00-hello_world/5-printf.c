2426a540b9d1f239cd1041ad7805c02a0cb72425
mayrhiarm <mariamsanni93@gmail.com>
2023-03-16 10:35:49 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A printf function program
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
