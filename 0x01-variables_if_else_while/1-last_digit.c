abb2b82260aebd7de47b9a9d63ca2607dae056c3
mayrhiarm <mariamsanni93@gmail.com>
2023-03-17 09:59:52 -0700

    last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /**
- * main - Print a the last digit of number
+ * main - Print the last digit of number
  * Return: Always (success)
  *
  */
@@ -21,14 +21,15 @@ int main(void)
 
 	if (lastd > 5)
 	{
-	printf("Last digit of %d is %d and and is greater than 5\n", n, lastd);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	{
-	printf("Last digit of %d id %d and is 0\n", n, lastd);
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
+}
