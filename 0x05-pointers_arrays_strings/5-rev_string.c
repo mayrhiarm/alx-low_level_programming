964327e28de1c3e2ab5590c4541550c51f9f764a
mayrhiarm <mariamsanni93@gmail.com>
2023-03-27 08:43:01 -0700

    reverses strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,7 @@ void rev_string(char *s)
 	int i, j;
 	char temp;
 
-	for (i = 0; j = string_length - 1; i < j; i++, j--)
+	for (i = 0, j = string_length - 1; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
