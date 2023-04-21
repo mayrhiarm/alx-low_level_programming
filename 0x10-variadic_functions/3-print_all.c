179ffdab6b401679f2f388389e3a9e9b585132b8
mayrhiarm <mariamsanni93@gmail.com>
2023-04-20 17:14:22 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,59 +4,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_c -  a function that prints char
- * @a: the list to give
- * Return: always 0 (success)
- */
-int print_c(va_list a)
-{
-	printf("%c", va_arg(a, int));
-	return (0);
-}
-
-/**
- * print_i - a function that prints int
- * @a: the list to give
- * Return: always 0 (success)
- */
-
-int print_i(va_list a)
-{
-	printf("%d", va_arg(a, int));
-	return (0);
-}
-
-/**
- * print_f - a function that prints float
- * @a: the list to give
- * Return: always 0 (success)
- */
-int print_f(va_list a)
-{
-	printf("%f", va_arg(a, double));
-	return (0);
-}
-
-/**
- * print_s - a function that prints string
- * @a: the list to give
- * Return: always 0 (success)
- */
-int print_s(va_list a)
-{
-	char *s;
-
-	s = va_arg(a, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return (0);
-	}
-	printf("%s", s);
-	return (0);
-}
-
-/**
  * print_all - a function that prints all
  * @format: list of the arg types
  *
@@ -66,15 +13,16 @@ void print_all(const char * const format, ...)
 	int f = 0;
 	char *str, *sep = "";
 
-	va_list list;
 
-	va_start(list, format);
+	va_list list;
+		va_start(list, format);
+
 
 	if (format)
-	{
+		{
 		while (format[f])
 		{
-			switch (format[f])
+		switch (format[f])
 			{
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
@@ -100,6 +48,6 @@ void print_all(const char * const format, ...)
 		}
 	}
 
-	printf("\n");
-	va_end(list);
+		printf("\n");
+		va_end(list);
 }
